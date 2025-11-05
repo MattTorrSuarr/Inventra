aa824b55fa01c54a3464769abfeece080d22d1fe
Matias Torres <matiastorressurez1@gmail.com>
2025-11-04 21:11:04 -0600

    new

--- gestionador/app/page.tsx
+++ gestionador/app/page.tsx
@@ -1,11 +1,13 @@
 import { ShoppingCart, Plane, TriangleAlert, Calculator, Banknote, Clock, Briefcase, ChevronRight  } from 'lucide-react';
 import { ChartNoAxesColumn } from 'lucide-react';
 import Link from "next/link";
-
+import { BackgroundRippleEffect } from '@/components/ui/background-ripple-effect';
 
 export default function Home() {
   return (
+    
     <div className="bg-black">
+      
 <header className="flex border-b border-[#242424] w-full h-[70px] bg-[#0a0a0a] items-center text-white px-5">
   <img src="/il.png" alt="My image" height={40} width={40} />
   <Link href="/">
@@ -32,10 +34,14 @@ export default function Home() {
   </div>
 </header>
 
-     <div className="text-white text-left font-semibold text-[80px] mt-[100px] mr-250 ml-auto max-w-[800px]">
-      <p className="flex items-center justify-center text-[15px] h-[30px] w-[200px] font-normal rounded-full bg-[#1a1a1a] text-[#8b8b8b]">Soporte 24/7</p>
-     <h5>Un software para la gestión de inventario de productos</h5>
-     <p className="text-[20px] font-light  mt-[20px] text-[#8b8b8b] ">Inventra es una herramienta de software diseñada para empresas que buscan tener un mayor control de su inventario y productos de forma eficiente y automatizada.
+<div className="flex flex-col items-center text-white text-center font-semibold text-[80px] mt-[100px] mx-auto max-w-[800px]">
+  <p className="text-[15px] h-[30px] w-[200px] font-normal rounded-full bg-[#1a1a1a] text-[#8b8b8b] flex items-center justify-center mb-4">
+    Soporte 24/7
+  </p>
+  <h5 className="max-w-[10000px] text-[70px] font-medium bg-gradient-to-r from-white to-grey bg-clip-text text-transparent">
+Usa <span className=''>Inventra  </span> para gestionar tu inventario  </h5>
+
+     <p className="text-[20px] font-light  mt-[20px] text-[#8b8b8b]  text-center">Inventra es una herramienta de software diseñada para empresas que buscan tener un mayor control de su inventario y productos de forma eficiente y automatizada.
 Moderniza tu negocio implementando tecnología sin gastar de más.</p>
 <div className="flex mt-10 gap-5">
   <Link href="/formulario">
@@ -97,28 +103,31 @@ Agendar demo
       </div>
 <Link href={"/formulario"}><button className="mt-[75px] w-[250px] h-[100px] bg-[#1f1f1f] rounded-[10px] font-medium border border-[#3d3d3d] mx-auto flex items-center justify-center">Agendar demo</button></Link>
 
-     </div>
-     <div className="text-left text-white ">
-      <h1 className="text-center mt-[80px] text-[50px] font-medium  text-left ml-[500px] mr-auto  max-w-[500px]">¿Como funciona?</h1>
-      <p className="mt-[10px] text-[#8b8b8b] text-[20px] text-left ml-[500px] mr-auto max-w-[500px]">Somos una empresa de tecnología mexicana especializada en el desarrollo de sistemas inteligentes para la gestión de inventarios.
-Nuestro software te ayuda a organizar, controlar y optimizar tus productos de forma automatizada, facilitando la toma de decisiones y mejorando la eficiencia de tu negocio.  </p>
-      <div className="flex items-center ml-[500px] mt-[10px] space-x-4">
-  <Link href="/formulario">
-    <button className="mt-[10px] text-[#8b8b8b] w-[250px] h-[100px] bg-[#1f1f1f] rounded-[10px] font-medium border border-[#3d3d3d] flex items-center justify-center">
-      Agendar demo
-    </button>
-  </Link>
+ <div className="text-left text-white ">
+  <h1 className="text-center mt-[80px] text-[50px] font-medium ml-auto mr-auto max-w-[500px]">
+    ¿Cómo funciona?
+  </h1>
 
-  <Link href="/formulario">
-    <button className="text-[#ffffff] w-[250px] h-[100px] rounded-[10px] font-medium flex items-center justify-center">
-      <span className="mt-[10px] flex items-center gap-2 hover:bg-[#131313] transition  w-[250px] h-[100px] justify-center rounded-[10px]">
+  <p className="mt-[10px] text-[#8b8b8b] text-[20px] text-center ml-auto mr-auto max-w-[500px]">
+    Somos una empresa de tecnología mexicana especializada en el desarrollo de sistemas inteligentes para la gestión de inventarios.
+    Nuestro software te ayuda a organizar, controlar y optimizar tus productos de forma automatizada, facilitando la toma de decisiones y mejorando la eficiencia de tu negocio.
+  </p>
+
+  <div className="flex items-center justify-center mt-[20px] gap-4">
+    <Link href="/formulario">
+      <button className="h-[70px] w-[200px] bg-[#ffffffdd] text-black rounded-[10px] font-medium text-[18px] flex items-center justify-center transition hover:bg-white">
+        Agendar demo
+      </button>
+    </Link>
+
+    <Link href="/About">
+      <button className="h-[70px] w-[200px] border border-[#3d3d3d] text-[#ffffffdd] rounded-[10px] font-medium text-[18px] flex items-center justify-center gap-2 hover:bg-[#131313] transition">
         Conoce más
         <ChevronRight height={20} width={20} className="text-white" />
-      </span>
-    </button>
-  </Link>
+      </button>
+    </Link>
+  </div>
 </div>
-
 
      </div>
      <div className="text-center mx-auto max-w-[500px] mt-[80px] text-[40px] text-[#8b8b8b] font-medium">
@@ -151,7 +160,7 @@ Nuestros sistemas automáticos realizan gran parte del trabajo por ti, permitié
 
 </div>
 
-      <div className="flex border-t border-[#242424] w-full h-[70px] bg-[#0a0a0a]  items-center  mt-80 ">
+      <div className="flex border-t border-[#242424] w-full h-[70px] bg-[#0a0a0a]  items-center  mt-[150px] ">
         <h1 className="text-[#8b8b8b] ml-4">© 2025 Inventra. All rights reserved.</h1>
       </div>
     </div>
